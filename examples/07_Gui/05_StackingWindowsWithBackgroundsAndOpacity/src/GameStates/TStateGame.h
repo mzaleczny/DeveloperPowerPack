82136c78008ef316ca90b5c4067fe53bbf8c5a0b
Marcin Załęczny <mzaleczny@gmail.com>
2026-04-15 12:01:00 +0200

    Added sample program demonstrating way of displaying backgrounds in windows and setting their transparency.

---
+++ examples/07_Gui/05_StackingWindowsWithBackgroundsAndOpacity/src/GameStates/TStateGame.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Tilc/Tilc.h"
+#include "Tilc/StateManager.h"
+
+namespace Tilc::Gui
+{
+	class TTextBox;
+	class TStyledWindow;
+}
+class TStateGame : public Tilc::TBaseState
+{
+public:
+	TStateGame(Tilc::TStateManager* StateManager);
+	virtual ~TStateGame();
+	virtual void OnCreate() override;
+	virtual void OnDestroy() override;
+	virtual void Activate() override {};
+	virtual void Deactivate() override {};
+	virtual void Update(float DeltaTime) override; // DeltaTime czas w sekundach
+	virtual void Draw() override;
+
+	void KeyEscapePressed(Tilc::TEventDetails* Details);
+
+protected:
+    Tilc::Gui::TTextBox* m_TextBox{};
+	float m_Elapsed;
+    void CreateButtonsInWindow(Tilc::Gui::TStyledWindow* wnd);
+};
