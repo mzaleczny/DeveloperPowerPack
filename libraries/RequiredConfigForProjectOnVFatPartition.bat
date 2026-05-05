4b4afde04376c3d64385cdf3720342cd96eb33b1
Marcin Załęczny <mzaleczny@gmail.com>
2026-05-05 15:03:13 +0200

    Added dependency on assimp and automatical resolving it. Added workarounds for config of glm library as assimp broke our config and prevented building Tilc lib properly.

--- libraries/RequiredConfigForProjectOnVFatPartition.bat
+++ libraries/RequiredConfigForProjectOnVFatPartition.bat
@@ -1,8 +1,8 @@
 @echo off
 
 SET OUTDIR=%CD:\=/%/out
-SET SAFEDIR_ZLIB_DEBUG=%OUTDIR%/build/x64-Debug/_deps/external-zlib-src
-SET SAFEDIR_ZLIB_RELEASE=%OUTDIR%/build/x64-Release/_deps/external-zlib-src
+SET SAFEDIR_ZLIB_DEBUG=%OUTDIR%/build/x64-Debug/_deps/external_zlib-src
+SET SAFEDIR_ZLIB_RELEASE=%OUTDIR%/build/x64-Release/_deps/external_zlib-src
 mkdir "%SAFEDIR_ZLIB_DEBUG%" 2>nul
 mkdir "%SAFEDIR_ZLIB_RELEASE%" 2>nul
 git config --global --get safe.directory "%SAFEDIR_ZLIB_DEBUG%" >nul
@@ -16,8 +16,8 @@ if NOT %ERRORLEVEL% equ 0 (
 
 
 
-SET SAFEDIR_GLM_DEBUG=%OUTDIR%/build/x64-Debug/_deps/glm-src
-SET SAFEDIR_GLM_RELEASE=%OUTDIR%/build/x64-Release/_deps/glm-src
+SET SAFEDIR_GLM_DEBUG=%OUTDIR%/build/x64-Debug/_deps/external_glm-src
+SET SAFEDIR_GLM_RELEASE=%OUTDIR%/build/x64-Release/_deps/external_glm-src
 mkdir "%SAFEDIR_GLM_DEBUG%" 2>nul
 mkdir "%SAFEDIR_GLM_RELEASE%" 2>nul
 git config --global --get safe.directory "%SAFEDIR_GLM_DEBUG%" >nul
@@ -30,3 +30,16 @@ if NOT %ERRORLEVEL% equ 0 (
 )
 
 
+
+SET SAFEDIR_ASSIMP_DEBUG=%OUTDIR%/build/x64-Debug/_deps/external_assimp-src
+SET SAFEDIR_ASSIMP_RELEASE=%OUTDIR%/build/x64-Release/_deps/external_assimp-src
+mkdir "%SAFEDIR_ASSIMP_DEBUG%" 2>nul
+mkdir "%SAFEDIR_ASSIMP_RELEASE%" 2>nul
+git config --global --get safe.directory "%SAFEDIR_ASSIMP_DEBUG%" >nul
+if NOT %ERRORLEVEL% equ 0 (
+    git config --global --add safe.directory "%SAFEDIR_ASSIMP_DEBUG%"
+)
+git config --global --get safe.directory "%SAFEDIR_ASSIMP_RELEASE%" >nul
+if NOT %ERRORLEVEL% equ 0 (
+    git config --global --add safe.directory "%SAFEDIR_ASSIMP_RELEASE%"
+)
--- libraries/RevertConfigForProjectOnVFatPartition.bat
+++ libraries/RevertConfigForProjectOnVFatPartition.bat
@@ -1,13 +1,19 @@
 @echo off
 
 SET OUTDIR=%CD:\=/%/out
-SET SAFEDIR_ZLIB_DEBUG=%OUTDIR%/build/x64-Debug/_deps/external-zlib-src
-SET SAFEDIR_ZLIB_RELEASE=%OUTDIR%/build/x64-Release/_deps/external-zlib-src
+SET SAFEDIR_ZLIB_DEBUG=%OUTDIR%/build/x64-Debug/_deps/external_zlib-src
+SET SAFEDIR_ZLIB_RELEASE=%OUTDIR%/build/x64-Release/_deps/external_zlib-src
 git config --global --unset safe.directory "%SAFEDIR_ZLIB_DEBUG%"
 git config --global --unset safe.directory "%SAFEDIR_ZLIB_RELEASE%"
 
 
-SET SAFEDIR_GLM_DEBUG=%OUTDIR%/build/x64-Debug/_deps/glm-src
-SET SAFEDIR_GLM_RELEASE=%OUTDIR%/build/x64-Release/_deps/glm-src
+SET SAFEDIR_GLM_DEBUG=%OUTDIR%/build/x64-Debug/_deps/external_glm-src
+SET SAFEDIR_GLM_RELEASE=%OUTDIR%/build/x64-Release/_deps/external_glm-src
 git config --global --unset safe.directory "%SAFEDIR_GLM_DEBUG%"
 git config --global --unset safe.directory "%SAFEDIR_GLM_RELEASE%"
+
+
+SET SAFEDIR_ASSIMP_DEBUG=%OUTDIR%/build/x64-Debug/_deps/external_assimp-src
+SET SAFEDIR_ASSIMP_RELEASE=%OUTDIR%/build/x64-Release/_deps/external_assimp-src
+git config --global --unset safe.directory "%SAFEDIR_ASSIMP_DEBUG%"
+git config --global --unset safe.directory "%SAFEDIR_ASSIMP_RELEASE%"
