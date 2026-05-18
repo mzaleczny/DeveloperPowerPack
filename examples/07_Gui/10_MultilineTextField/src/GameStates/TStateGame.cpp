dd500097c0e0a52c10eecbadf1aa5e3d58714fde
Marcin Załęczny <mzaleczny@gmail.com>
2026-05-18 13:54:21 +0200

    Minor mofification of tested multiline text.

--- examples/07_Gui/10_MultilineTextField/src/GameStates/TStateGame.cpp
+++ examples/07_Gui/10_MultilineTextField/src/GameStates/TStateGame.cpp
@@ -52,15 +52,16 @@ TStateGame::TStateGame(Tilc::TStateManager* StateManager) : Tilc::TBaseState(Sta
     Y += 30.0f;
     txt = new Tilc::Gui::TTextField(wnd, "Text Field 10", { X, Y, 220, 25 }, "Wartość 10");
     Y += 30.0f;
-    txt = new Tilc::Gui::TTextField(wnd, "Text Field 11", { X, Y, 220, 25 }, "Wartość 11");
+    txt = new Tilc::Gui::TTextField(wnd, "Text Field 11", { X, Y, 220, 25 }, "Linijka 11");
     Y += 30.0f;
     
     Tilc::Gui::TMultilineTextField* memo;
     memo = new Tilc::Gui::TMultilineTextField(wnd, "Multiline Text Field 1", { 260, 60, 480, 325 },
-        "Linijka 1\nWartość 1 zawierająca długaśny tekst mający nam ułatwić testowanie zaznaczenia w kontrolce tekstowej. I trochę polskich literek: ąćżęńćłó ĄĆĘŁÓŃŚŹŻ."
+        "Linijka 1\nLniana koszula filozofa\nWartość 1 zawierająca długaśny tekst mający nam ułatwić testowanie zaznaczenia w kontrolce tekstowej. I trochę polskich literek: ąćżęńćłó ĄĆĘŁÓŃŚŹŻ."
         " Wartość 1 zawierająca długaśny tekst mający nam ułatwić testowanie zaznaczenia w kontrolce tekstowej. I trochę polskich literek: ąćżęńćłó ĄĆĘŁÓŃŚŹŻ."
         " Wartość 1 zawierająca długaśny tekst mający nam ułatwić testowanie zaznaczenia w kontrolce tekstowej. I trochę polskich literek: ąćżęńćłó ĄĆĘŁÓŃŚŹŻ."
-        " Wartość 1 zawierająca długaśny tekst mający nam ułatwić testowanie zaznaczenia w kontrolce tekstowej. I trochę polskich literek: ąćżęńćłó ĄĆĘŁÓŃŚŹŻ."
+        " Wartość 1 zawierająca długaśny tekst mający nam ułatwić testowanie zaznaczenia w kontrolce tekstowej. I trochę polskich literek: ąćżęńćłó ĄĆĘŁÓŃŚŹŻ.\n"
+        "Linux/Windows/Android"
     );
 
     Tilc::Gui::TButton* btnSave = new Tilc::Gui::TButton(wnd, "SaveButton", { X, Y, 100, 25 }, "Zapisz");
