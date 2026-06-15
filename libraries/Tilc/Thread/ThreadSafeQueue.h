3578109690b74e023c65170747feb8f010b3e57c
Marcin Załęczny <mzaleczny@gmail.com>
2026-06-15 09:53:07 +0200

    Added Tilc::Thread namespace

--- libraries/Tilc/Thread/ThreadSafeQueue.h
+++ libraries/Tilc/Thread/ThreadSafeQueue.h
@@ -3,68 +3,73 @@
 #include <condition_variable>
 #include <memory>
 
-template<typename T>
-class TThreadSafeQueue
+namespace Tilc::Thread
 {
-private:
-    mutable std::mutex m_Mutex;
-    std::queue<T> m_DataQueue;
-    std::condition_variable m_DataCond;
-public:
-    TThreadSafeQueue()
-    {}
 
-    void Push(T new_value)
+    template<typename T>
+    class TThreadSafeQueue
     {
-        std::lock_guard<std::mutex> lk(m_Mutex);
-        m_DataQueue.push(std::move(new_value));
-        m_DataCond.notify_one();
-    }
+    private:
+        mutable std::mutex m_Mutex;
+        std::queue<T> m_DataQueue;
+        std::condition_variable m_DataCond;
+    public:
+        TThreadSafeQueue()
+        {}
 
-    void WaitAndPop(T& value)
-    {
-        std::unique_lock<std::mutex> lk(m_Mutex);
-        m_DataCond.wait(lk, [this] { return !m_DataQueue.empty(); });
-        value = std::move(m_DataQueue.front());
-        m_DataQueue.pop();
-    }
-
-    std::shared_ptr<T> WaitAndPop()
-    {
-        std::unique_lock<std::mutex> lk(m_Mutex);
-        m_DataCond.wait(lk, [this] { return !m_DataQueue.empty(); });
-        std::shared_ptr<T> res(std::make_shared<T>(std::move(m_DataQueue.front())));
-        m_DataQueue.pop();
-        return res;
-    }
-
-    bool TryPop(T& value)
-    {
-        std::lock_guard<std::mutex> lk(m_Mutex);
-        if (m_DataQueue.empty())
+        void Push(T new_value)
         {
-            return false;
+            std::lock_guard<std::mutex> lk(m_Mutex);
+            m_DataQueue.push(std::move(new_value));
+            m_DataCond.notify_one();
         }
-        value = std::move(m_DataQueue.front());
-        m_DataQueue.pop();
-        return true;
-    }
 
-    std::shared_ptr<T> TryPop()
-    {
-        std::lock_guard<std::mutex> lk(m_Mutex);
-        if (m_DataQueue.empty())
+        void WaitAndPop(T& value)
         {
-            return std::shared_ptr<T>();
+            std::unique_lock<std::mutex> lk(m_Mutex);
+            m_DataCond.wait(lk, [this] { return !m_DataQueue.empty(); });
+            value = std::move(m_DataQueue.front());
+            m_DataQueue.pop();
         }
-        std::shared_ptr<T> res(std::make_shared<T>(std::move(m_DataQueue.front())));
-        m_DataQueue.pop();
-        return res;
-    }
 
-    bool Empty() const
-    {
-        std::lock_guard<std::mutex> lk(m_Mutex);
-        return m_DataQueue.empty();
-    }
-};
+        std::shared_ptr<T> WaitAndPop()
+        {
+            std::unique_lock<std::mutex> lk(m_Mutex);
+            m_DataCond.wait(lk, [this] { return !m_DataQueue.empty(); });
+            std::shared_ptr<T> res(std::make_shared<T>(std::move(m_DataQueue.front())));
+            m_DataQueue.pop();
+            return res;
+        }
+
+        bool TryPop(T& value)
+        {
+            std::lock_guard<std::mutex> lk(m_Mutex);
+            if (m_DataQueue.empty())
+            {
+                return false;
+            }
+            value = std::move(m_DataQueue.front());
+            m_DataQueue.pop();
+            return true;
+        }
+
+        std::shared_ptr<T> TryPop()
+        {
+            std::lock_guard<std::mutex> lk(m_Mutex);
+            if (m_DataQueue.empty())
+            {
+                return std::shared_ptr<T>();
+            }
+            std::shared_ptr<T> res(std::make_shared<T>(std::move(m_DataQueue.front())));
+            m_DataQueue.pop();
+            return res;
+        }
+
+        bool Empty() const
+        {
+            std::lock_guard<std::mutex> lk(m_Mutex);
+            return m_DataQueue.empty();
+        }
+    };
+
+}
