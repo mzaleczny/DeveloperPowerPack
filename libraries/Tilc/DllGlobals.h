851253cbdf769cebe1cd4dcc751289b86a954e6b
Marcin Załęczny <mzaleczny@gmail.com>
2026-06-15 14:23:20 +0200

    Added skipping defining DECLSPEC if one is already set.

--- libraries/Tilc/DllGlobals.h
+++ libraries/Tilc/DllGlobals.h
@@ -1,11 +1,13 @@
 #pragma once
 
-#if defined(_WIN32)
-#  if defined(DLL_EXPORT)
-#    define DECLSPEC __declspec(dllexport)
-#  else
-#    define DECLSPEC __declspec(dllimport)
-#  endif
-#else // non windows
-#  define DECLSPEC
+#ifndef DECLSPEC
+    #if defined(_WIN32)
+        #if defined(DLL_EXPORT)
+            #define DECLSPEC __declspec(dllexport)
+        #else
+            #define DECLSPEC __declspec(dllimport)
+        #endif
+    #else // non windows
+        #define DECLSPEC
+    #endif
 #endif
