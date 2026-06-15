fbb37a2bdb219d23f638a37dfa26d0220e00506b
Marcin Załęczny <mzaleczny@gmail.com>
2026-06-15 09:54:14 +0200

    Added Tilc::Thread namespace and slight changes marking when all threads are added to pool.

--- libraries/Tilc/Thread/ThreadPool.h
+++ libraries/Tilc/Thread/ThreadPool.h
@@ -5,82 +5,88 @@
 #include <future>
 #include "Tilc/Thread/ThreadSafeQueue.h"
 
-class TJoinThreads
+namespace Tilc::Thread
 {
-    std::vector<std::thread>& m_Threads;
-public:
-    explicit TJoinThreads(std::vector<std::thread>& threads) :
-        m_Threads(threads)
-    {}
-    ~TJoinThreads()
+    class TJoinThreads
     {
-        for (unsigned long i = 0; i < m_Threads.size(); ++i)
+        std::vector<std::thread>& m_Threads;
+    public:
+        explicit TJoinThreads(std::vector<std::thread>& threads) :
+            m_Threads(threads)
+        {}
+        ~TJoinThreads()
         {
-            if (m_Threads[i].joinable())
+            for (unsigned long i = 0; i < m_Threads.size(); ++i)
             {
-                m_Threads[i].join();
-            }
-        }
-    }
-};
-
-class TThreadPool
-{
-private:
-    std::atomic_bool m_Done;
-    std::atomic_bool m_WorkStarted;
-    TThreadSafeQueue<std::function<void()>> m_WorkQueue;
-    std::vector<std::thread> m_Threads;
-    TJoinThreads m_Joiner;
-
-    void WorkerThread()
-    {
-        while (!m_Done)
-        {
-            std::function<void()> task;
-            if(m_WorkQueue.TryPop(task))
-            {
-                m_WorkStarted = true;
-                task();
-            }
-            else
-            {
-                std::this_thread::yield();
-                if (m_WorkStarted)
+                if (m_Threads[i].joinable())
                 {
-                    m_Done = true;
+                    m_Threads[i].join();
                 }
             }
         }
-    }
-public:
-    TThreadPool():
-        m_Done(false), m_Joiner(m_Threads), m_WorkStarted(false)
+    };
+
+    class TThreadPool
     {
-        unsigned const ThreadCount = std::thread::hardware_concurrency();
-        try
+    private:
+        std::atomic_bool m_Done;
+        std::atomic_bool m_AllThreadsAdded;
+        TThreadSafeQueue<std::function<void()>> m_WorkQueue;
+        std::vector<std::thread> m_Threads;
+        TJoinThreads m_Joiner;
+
+        void WorkerThread()
         {
-            for(unsigned i = 0; i < ThreadCount; ++i)
+            while (!m_Done)
             {
-                m_Threads.push_back(std::thread(&TThreadPool::WorkerThread, this));
+                std::function<void()> task;
+                if (m_WorkQueue.TryPop(task))
+                {
+                    task();
+                }
+                else
+                {
+                    if (m_AllThreadsAdded)
+                    {
+                        m_Done = true;
+                    }
+                    else
+                    {
+                        std::this_thread::yield();
+                    }
+                }
             }
         }
-        catch (...)
+    public:
+        TThreadPool() :
+            m_Done(false), m_Joiner(m_Threads), m_AllThreadsAdded(false)
+        {
+            unsigned const ThreadCount = std::thread::hardware_concurrency();
+            try
+            {
+                for (unsigned i = 0; i < ThreadCount; ++i)
+                {
+                    m_Threads.push_back(std::thread(&TThreadPool::WorkerThread, this));
+                }
+            }
+            catch (...)
+            {
+                m_Done = true;
+            }
+        }
+
+        ~TThreadPool()
         {
             m_Done = true;
         }
-    }
 
-    ~TThreadPool()
-    {
-        m_Done = true;
-    }
+        bool IsDone() const { return m_Done.load(); }
+        bool AllThreadsAdded() { return m_AllThreadsAdded = true; }
 
-    bool IsDone() const { return m_Done.load(); }
-
-    template<typename FunctionType>
-    void submit(FunctionType f)
-    {
-        m_WorkQueue.Push(std::function<void()>(f));
-    }
-};
+        template<typename FunctionType>
+        void submit(FunctionType f)
+        {
+            m_WorkQueue.Push(std::function<void()>(f));
+        }
+    };
+}
