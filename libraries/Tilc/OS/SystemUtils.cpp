28c935bad074c7589f1d0c92b28976584993366a
Marcin Załęczny <mzaleczny@gmail.com>
2026-06-15 09:32:53 +0200

    Added function nanosleeep for windows.

--- libraries/Tilc/OS/SystemUtils.cpp
+++ libraries/Tilc/OS/SystemUtils.cpp
@@ -6,10 +6,11 @@
 
 #ifdef _WINDOWS
 #include <Windows.h>
+#include <errno.h>
 #endif
 
 
-void* Tilc::OS::GetActiveWindowSystemHandle()
+DECLSPEC void* Tilc::OS::GetActiveWindowSystemHandle()
 {
 #ifdef _WINDOWS
     return ::GetActiveWindow();
@@ -17,5 +18,57 @@ void* Tilc::OS::GetActiveWindowSystemHandle()
     return nullptr;
 #else
     return nullptr;
+#endif
+}
+
+
+
+DECLSPEC int Tilc::OS::nanosleep(const timespec* req, timespec* rem)
+{
+#ifdef _WINDOWS
+    if (!req || req->tv_nsec < 0 || req->tv_nsec >= 1000000000L)
+    {
+        return EINVAL;
+    }
+
+    LONGLONG ticks = -(
+        static_cast<LONGLONG>(req->tv_sec) * 10000000LL +
+        static_cast<LONGLONG>(req->tv_nsec) / 100LL
+    );
+    HANDLE timer = CreateWaitableTimerA(NULL, TRUE, NULL);
+    if (!timer)
+    {
+        return -1;
+    }
+
+    LARGE_INTEGER li;
+    li.QuadPart = ticks;
+
+    if (!SetWaitableTimer(timer, &li, 0, NULL, NULL, FALSE))
+    {
+        CloseHandle(timer);
+        return -1;
+    }
+
+    DWORD result = WaitForSingleObject(timer, INFINITE);
+    CloseHandle(timer);
+
+    if (result == WAIT_OBJECT_0)
+    {
+        return 0;
+    }
+
+    if (rem)
+    {
+        rem->tv_sec = 0;
+        rem->tv_nsec = 0;
+    }
+
+    errno = EINTR;
+    return -1;
+#elif ANDROID
+    return 0;
+#else
+    nanosleep(req, rem);
 #endif
 }
--- libraries/Tilc/OS/SystemUtils.h
+++ libraries/Tilc/OS/SystemUtils.h
@@ -1,7 +1,20 @@
 #pragma once
 #include "Tilc/DllGlobals.h"
 
+#ifdef _WINDOWS
+#include <time.h>
+#endif
+
 namespace Tilc::OS
 {
+#ifdef _WINDOWS
+    struct timespec
+    {
+        time_t tv_sec; // sekundy
+        long tv_nsec;  // nanosekundy
+    };
+#endif
+
     DECLSPEC void* GetActiveWindowSystemHandle();
+    DECLSPEC int nanosleep(const struct timespec* req, struct timespec* rem);
 }
