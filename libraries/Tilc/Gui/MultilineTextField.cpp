5c2905cd7e3aafee3b63b69100448a6b20c5eaf8
Marcin Załęczny <mzaleczny@gmail.com>
2026-06-19 10:15:04 +0200

    Fixed error during type-in new text in lines when top line is not 0 - when text is scrolled down.

--- libraries/Tilc/Gui/MultilineTextField.cpp
+++ libraries/Tilc/Gui/MultilineTextField.cpp
@@ -1309,7 +1309,9 @@ bool Tilc::Gui::TMultilineTextField::OnTextInput(const SDL_Event& event)
 void Tilc::Gui::TMultilineTextField::RedrawLineInTextTextureBuffer(int LineNumber)
 {
     // Przy określaniu linii uwzględniamy zmienną TopLine, żeby móc wskazywać linie nie mieszczące się w widoku kontrolki
-    SDL_Texture* TextLineTexture = m_HbTextLayoutCache->RenderHbLineToTexture(Renderer, m_TopLine + LineNumber);
+    SDL_Texture* TextLineTexture = m_HbTextLayoutCache->RenderHbLineToTexture(Renderer, LineNumber);
+    // Set Line number relative to top line to properly manipulate rendered texture
+    LineNumber -= m_TopLine;
     if (TextLineTexture)
     {
         // Rysujemy tło i tekst
