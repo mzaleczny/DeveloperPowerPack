8c5401075185c9c689d124c23dd677f041328c72
Marcin Załęczny <mzaleczny@gmail.com>
2026-07-09 16:15:21 +0200

    Move config variables to configuration script. Added config variable for conditional compilation of mysql/mariadb support.

--- libraries/TilcConfigure.cmake
+++ libraries/TilcConfigure.cmake
@@ -1,2 +1,11 @@
 set(FORCE_OPENGL_ES "1")
+set(BUILD_WITH_MYSQL "1")
+
+set_property(GLOBAL PROPERTY Tilc_HEADERS_Property "")
+set_property(GLOBAL PROPERTY Tilc_SOURCES_Property "")
+set_property(GLOBAL PROPERTY Tilc_PLATFORM_SPECIFIC_HEADERS_Property "")
+set_property(GLOBAL PROPERTY Tilc_PLATFORM_SPECIFIC_SOURCES_Property "")
+set_property(GLOBAL PROPERTY BuildWithMySQL BUILD_WITH_MYSQL)
+
+
 configure_file(Tilc/configure.h.in configure.h)
