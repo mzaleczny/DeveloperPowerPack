c39e8350f4c3d693e6e9b1f5064d8f17cfb57407
Marcin Załęczny <mzaleczny@gmail.com>
2026-07-24 14:06:03 +0200

    Turn off building mysql support in default configuration. Preparations for support server-side build.

--- libraries/TilcConfigure.cmake
+++ libraries/TilcConfigure.cmake
@@ -1,11 +1,13 @@
-set(FORCE_OPENGL_ES "1")
-set(BUILD_WITH_MYSQL "1")
+set(FORCE_OPENGL_ES "0")
+set(BUILD_WITH_MYSQL "0")
+set(BUILD_FOR_SERVER "0")
 
 set_property(GLOBAL PROPERTY Tilc_HEADERS_Property "")
 set_property(GLOBAL PROPERTY Tilc_SOURCES_Property "")
 set_property(GLOBAL PROPERTY Tilc_PLATFORM_SPECIFIC_HEADERS_Property "")
 set_property(GLOBAL PROPERTY Tilc_PLATFORM_SPECIFIC_SOURCES_Property "")
 set_property(GLOBAL PROPERTY BuildWithMySQL BUILD_WITH_MYSQL)
+set_property(GLOBAL PROPERTY BuildForServer BUILD_FOR_SERVER)
 
 
 configure_file(Tilc/configure.h.in configure.h)
