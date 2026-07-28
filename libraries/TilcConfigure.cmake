0355952cd7988488491d1662e5fd37a3f20eeb12
Marcin Załęczny <mzaleczny@gmail.com>
2026-07-28 14:55:35 +0200

    Added config for curl and binary directory containing visual studio dev tools. Added downloading and configuring precompiled bineries of curl library in windows systems.

--- libraries/TilcConfigure.cmake
+++ libraries/TilcConfigure.cmake
@@ -9,5 +9,11 @@ set_property(GLOBAL PROPERTY Tilc_PLATFORM_SPECIFIC_SOURCES_Property "")
 set_property(GLOBAL PROPERTY BuildWithMySQL BUILD_WITH_MYSQL)
 set_property(GLOBAL PROPERTY BuildForServer BUILD_FOR_SERVER)
 
+set(CURL_VERSION "curl-8.21.0_5-win64-mingw")
+set(URL_CURL_PRECOMPILED_BINARIES "https://curl.se/windows/dl-8.21.0_5/${CURL_VERSION}.zip")
+set(URL_CURL_EXPECTED_HASH "4c48761e9b70f447af76e65564cf2afbbf626e4ea6286008fde5cc068de237fd")
+
+# Below is location of lib.exe file needed to generate lib file for dll file. It is used for example for genrating curl.lib for curl.dll.
+set(VS_BINARIES "c:/Program Files/Microsoft Visual Studio/18/Community/VC/Tools/MSVC/14.51.36231/bin/Hostx64/x64")
 
 configure_file(Tilc/configure.h.in configure.h)
