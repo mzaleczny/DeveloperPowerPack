35d2fc41a05483a54a045d623c326441341a55eb
Marcin Załęczny <mzaleczny@gmail.com>
2026-07-29 14:27:49 +0200

    Added functionality to TMoney class.

--- libraries/Tilc/Commerce/Money.cpp
+++ libraries/Tilc/Commerce/Money.cpp
@@ -1,6 +1,217 @@
+#include "Tilc/Commerce/Money.h"
 #include "Tilc/Utils/Log.h"
 #include <fstream>
 
-namespace Tilc
+Tilc::Commerce::TMoney& Tilc::Commerce::TMoney::operator=(double Total)
 {
+    if (Total < 0)
+    {
+        return;
+    }//if
+
+    int a, d;
+    // np. 12.78
+    a = std::floor(Total); // części całkowite -> 12
+    Total = Total - a;   // pozostaw części ułamkowe -> 0.78
+    Total = Total * 100; // zrób z nich części całkowite -> 78
+    d = std::round(Total); // zwróć je jako grosze
+    if (d > 99)
+    {
+        a++;
+        d = 0;
+    }
+
+    m_Amount = a;
+    m_Decimals = d;
+
+    return *this;
 }
+
+Tilc::Commerce::TMoney& Tilc::Commerce::TMoney::operator=(Tilc::TExtString str)
+{
+    Tilc::TExtString tmp;
+    int i;
+
+    // sprawdzaj każdy znak danego łańcucha
+    for (i = 0; i < str.length(); ++i)
+    {
+        // Jeżeli jest spacją, to po prostu omiń
+        if (str[i] == ' ')
+        {
+            continue;
+        }
+        // i jeśli jest cyfrą, do doklejaj do tmp
+        else if (str[i] >= '0' && str[i] <= '9')
+        {
+            tmp += str[i];
+        }
+        // jeśli jest kropką, to wyjdź z pętli
+        else if (str[i] == m_Separator)
+        {
+            break;
+        }
+        else
+        {
+            m_Amount = -1;
+            m_Decimals = -1;
+            return;
+        }
+    }
+    // tmp zawiera teraz z│ote
+    m_Amount = std::atoi(tmp.c_str());
+
+
+    // ustaw Decimals
+    if (i + 1 < str.length())
+    {
+        tmp.assign(&str[i + 1], 1);
+        if (i + 2 < str.length())
+        {
+            tmp += str[i + 2];
+        }
+        else
+        {
+            tmp += "0";
+        }
+    }//if
+    else
+    {
+        tmp = "0";
+    }
+    m_Decimals = std::atoi(tmp.c_str());
+    return *this;
+}
+
+Tilc::Commerce::TMoney Tilc::Commerce::TMoney::operator+(Tilc::TExtString str)
+{
+    int a = m_Amount;
+    int d = m_Decimals;
+
+    Tilc::Commerce::TMoney m;
+    m = str;
+    if (m.m_Amount == -1 && m.m_Decimals == -1)
+    {
+        return *this;
+    }
+
+    m.m_Amount += a;
+    m.m_Decimals += d;
+    if (m_Decimals > 99)
+    {
+        ++m.m_Amount;
+        m.m_Decimals -= 100;
+    }
+    return m;
+}
+
+Tilc::Commerce::TMoney Tilc::Commerce::TMoney::operator-(Tilc::TExtString str)
+{
+    int a = m_Amount;
+    int d = m_Decimals;
+
+    Tilc::Commerce::TMoney m;
+    m = str;
+    if (m.m_Amount == -1 && m.m_Decimals == -1)
+    {
+        return *this;
+    }
+
+    int Total = GetTotalAmountInt() - m.GetTotalAmountInt();
+    m.SetTotal(Total);
+    return m;
+}
+
+Tilc::Commerce::TMoney Tilc::Commerce::TMoney::operator/(int i)
+{
+    double x;
+
+    if (i == 0) return {};
+    if (i == 1) return *this;
+
+    // W przeciwnym razie wykonaj przeliczenia
+    double Total = GetTotalAmount();
+    int Amount = std::floor(Total);
+    Total -= Amount;
+    Total *= 100;
+    int Decimals = Total;
+    if (Decimals >= 100)
+    {
+        Decimals -= 100;
+        ++Amount;
+    }//if
+
+    return Tilc::Commerce::TMoney(Amount, Decimals);
+}
+
+Tilc::TExtString Tilc::Commerce::TMoney::Procent(double i)
+{
+    double x;
+    x = (GetTotalAmount() * i) / 100.0;
+    SetTotal(x);
+    return ToString();
+}
+
+Tilc::TExtString Tilc::Commerce::TMoney::ToString() const
+{
+    Tilc::TExtString tmp;
+
+    tmp = std::to_string(m_Amount) + m_Separator;
+    if (m_Decimals < 10)
+    {
+        tmp += "0";
+    }
+    tmp += std::to_string(m_Amount);
+
+    return tmp;
+}
+
+Tilc::TExtString Tilc::Commerce::TMoney::ToSpeech() const
+{
+    return Tilc::SlowDziewiec(m_Amount) + " " + AmountCurrency + "  " + std::to_string(m_Decimals) + "/100 " + DecimalsCurrency;
+}
+
+Tilc::TExtString Tilc::Commerce::TMoney::GetFormattedKasa(Tilc::TExtString format) const
+{
+    Tilc::TExtString res = "";
+    Tilc::TExtString znak = " ";
+
+    int i = 0;
+
+    //Jeśli łańcuch zawierający format jest pusty, to ustaw domyślny format
+    if (format.empty())
+    {
+        format = "%z zł %g gr";
+    }
+
+    //Przejdź przez cały łańcuch formatu i odpowiednio go interpretuj
+    while (i < format.length())
+    {
+        if (format[i] == '%' && (i + 1 < format.length()))
+        {
+            ++i;
+            switch (format[i])
+            {
+            case '%':
+                res += "%"; //doklej ten znak do łańcucha wynikowego
+                break;
+            case 'z':
+            case 'Z':
+                res += std::to_string(m_Amount); //doklej kwotę do łańcucha wynikowego
+                break;
+            case 'g':
+            case 'G':
+                res += std::to_string(m_Decimals); //doklej grosze do łańcucha wynikowego
+                break;
+            }//switch
+        }//if
+        else
+        {
+            res += format[i];
+        }//else
+
+        ++i; //zwiększ licznik
+    }//while
+
+    return res;
+}
+
--- libraries/Tilc/Commerce/Money.h
+++ libraries/Tilc/Commerce/Money.h
@@ -3,38 +3,100 @@
 #include "Tilc/DllGlobals.h"
 #include "Tilc/Utils/ExtString.h"
 
-namespace Tilc::Commerce
+namespace Tilc
 {
+    namespace Commerce
+    {
 
-	class DECLSPEC TMoney
-	{
-	public:
-        TMoney() = default;
-		TMoney(double v) : m_Value(v) {}
-		void operator()(Tilc::TExtString Data, const Tilc::TExtString& AppendStr = "\n");
+        class DECLSPEC TMoney
+        {
+        public:
+            TMoney() {};
+            TMoney(int Amount, int Decimals = 0, char Separator = ',') : m_Amount(Amount), m_Decimals(Decimals), m_Separator(Separator) {};
+            ~TMoney() {}
 
-        double m_Value;
-	};
+            inline int GetAmount() const {
+                return m_Amount;
+            }
 
+            inline int GetDecimals() const {
+                return m_Decimals;
+            }
+
+            char GetSeparator() const {
+                return m_Separator;
+            }
+
+            inline int GetTotalAmountInt() const {
+                return m_Amount * 100 + +m_Decimals;
+            }
+
+            inline double GetTotalAmount() const {
+                return (double)m_Amount + (double)m_Decimals / 100;
+            }
+
+            inline void SetTotal(int Total) {
+                m_Amount = Total / 100;
+                m_Decimals = Total % 100;
+            }
+
+            inline void SetTotal(double Total) {
+                m_Amount = std::floor(Total);
+                m_Decimals = std::floor((Total-m_Amount) * 100);
+            }
+
+            inline void SetAmount(int Amount) {
+                m_Amount = Amount;
+            }
+
+            inline void SetDecimals(int Decimals)
+            {
+                m_Decimals = Decimals;
+            }
+
+            TMoney& operator=(double Total);
+            TMoney& operator=(Tilc::TExtString str);
+            TMoney operator+(Tilc::TExtString str);
+            TMoney operator-(Tilc::TExtString str);
+            TMoney operator/(int i);
+            // zwraca procent z aktualnej kwoty w formie łańcucha i równoczeťnie zmienia zawartość obiektu
+            Tilc::TExtString Procent(double i);
+
+            Tilc::TExtString ToString() const;
+            Tilc::TExtString ToSpeech() const;
+            Tilc::TExtString GetFormattedKasa(Tilc::TExtString format = "") const;
+
+        private:
+            //ilość
+            int m_Amount{};
+            //ilość po przecinku
+            int m_Decimals{};
+            //symbol reprezentujący kropkę dziesiętną
+            char m_Separator{ ',' };
+            char AmountCurrency[8]{ "zł" };
+            char DecimalsCurrency[8]{ "gr" };
+        };
+
+    }
 }
 
 inline bool operator==(const Tilc::Commerce::TMoney& m1, const Tilc::Commerce::TMoney& m2)
 {
-	return m1.m_Value == m2.m_Value;
+    return m1.GetAmount() == m2.GetAmount() && m1.GetDecimals() == m2.GetDecimals();
 }
 inline bool operator<(const Tilc::Commerce::TMoney& m1, const Tilc::Commerce::TMoney& m2)
 {
-	return m1.m_Value < m2.m_Value;
+    return m1.GetAmount() * 100 + m1.GetDecimals() < m2.GetAmount() * 100 + m2.GetDecimals();
 }
 inline bool operator>(const Tilc::Commerce::TMoney& m1, const Tilc::Commerce::TMoney& m2)
 {
-	return m1.m_Value > m2.m_Value;
+    return m1.GetAmount() * 100 + m1.GetDecimals() > m2.GetAmount() * 100 + m2.GetDecimals();
 }
 inline Tilc::Commerce::TMoney operator+(const Tilc::Commerce::TMoney& m1, const Tilc::Commerce::TMoney& m2)
 {
-	return Tilc::Commerce::TMoney{m1.m_Value + m2.m_Value};
+    return m1 + m2;
 }
 inline Tilc::Commerce::TMoney operator-(const Tilc::Commerce::TMoney& m1, const Tilc::Commerce::TMoney& m2)
 {
-	return Tilc::Commerce::TMoney{m1.m_Value - m2.m_Value};
+    return m1 - m2;
 }
