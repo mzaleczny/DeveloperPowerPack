c757c4f5c57d8043942edd59cb4108f5aba14bfb
Marcin Załęczny <mzaleczny@gmail.com>
2026-08-12 09:40:55 +0200

    Full parametrization of the build process. Now we can specify each subdependency should be processed.

--- libraries/Tilc/configure.h
+++ libraries/Tilc/configure.h
@@ -1,7 +1,15 @@
 #pragma once
 
+/* #undef BUILD_GAME_STUFF */
 /* #undef BUILD_WITH_OPENGL */
 /* #undef FORCE_OPENGL_ES */
-#define BUILD_WITH_MYSQL 1
-#define BUILD_WITHOUT_GRAPHICS 1
+#define BUILD_WITH_MARIADB 1
+#define BUILD_WITH_SDL3 1
+#define BUILD_WITH_SDL3_IMAGE 1
+#define BUILD_WITH_SDL3_TTF 1
+/* #undef BUILD_WITH_SDL3_MIXER */
+/* #undef BUILD_WITH_GLM */
+/* #undef BUILD_WITH_ASSIMP */
+#define BUILD_WITH_CURL 1
 #define BUILD_WITH_LIBFCGI 1
+#define BUILD_WITH_GUI 1
