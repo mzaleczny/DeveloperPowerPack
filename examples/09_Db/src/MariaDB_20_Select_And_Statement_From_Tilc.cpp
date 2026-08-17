9ad600599671361652fed5ababdac2ca4587c527
Marcin Załęczny <mzaleczny@gmail.com>
2026-08-17 15:14:20 +0200

    Fixes making sample programs demonstrating using mysql operations works properly.

--- examples/09_Db/src/MariaDB_20_Select_And_Statement_From_Tilc.cpp
+++ examples/09_Db/src/MariaDB_20_Select_And_Statement_From_Tilc.cpp
@@ -1,21 +1,47 @@
 #include <iostream>
+#include "Tilc/Tilc.h"
 #include "Tilc/Utils/ExtString.h"
 #include "Tilc/Data/DbMySQL.h"
 
-constexpr const char* HOST_NAME{ "192.168.56.2" };
+constexpr const char* HOST_NAME{ "192.168.56.102" };
 constexpr const char* DB_NAME{ "testdb" };
 constexpr const char* USER_NAME{ "testuser" };
 constexpr const char* PASSWD_NAME{ "testpasswd" };
 
 int main(int argc, char* argv[])
 {
-    Tilc::Data::TDBMySQL DB(HOST_NAME, DB_NAME, USER_NAME, PASSWD_NAME);
-    Tilc::Data::TDBDataRows Rows;
-    DB.Select("SELECT id, name, age FROM users", Rows);
-    for (size_t i = 0; i < Rows.size(); ++i)
+    Tilc::InitTilc();
+
+    // IMPORTANT!!! Variable Tilc::Data::TDBMySQL DB must be in inner scope, to do proper close db. Because if it would be in main scope, function Tilc::Cleanup
+    // would be called before DB destructor. And in destructor would be call to unloaded DLL.
     {
-        std::cout << Rows[i][0] << " | " << Rows[i][1] << " | " << Rows[i][2] << std::endl;
+        Tilc::Data::TDBMySQL DB(HOST_NAME, DB_NAME, USER_NAME, PASSWD_NAME);
+        Tilc::Data::TDBDataRows Rows;
+        DB.ExecQuery("DROP TABLE IF EXISTS users", {}, {});
+        DB.ExecQuery("CREATE TABLE users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), age INTEGER, dat DATE, tim TIME, dattim DATETIME, fract FLOAT, dbl DOUBLE, decim DECIMAL(10,2))", {}, {});
+        DB.Insert("INSERT INTO users (name, age, dat, tim, dattim, fract, dbl, decim) VALUES (?,?,?,?,?,?,?,?)",
+            { Tilc::Data::EDBFT_VARCHAR, Tilc::Data::EDBFT_INT, Tilc::Data::EDBFT_DATE, Tilc::Data::EDBFT_TIME, Tilc::Data::EDBFT_DATETIME, Tilc::Data::EDBFT_FLOAT, Tilc::Data::EDBFT_DOUBLE, Tilc::Data::EDBFT_DECIMAL},
+            { "TestowyUser #1", "11", "2026-01-01", "12:11:11", "2026-01-01 12:11:11", "11.11", "11.11111", "11111.11"}
+        );
+        DB.Insert("INSERT INTO users (name, age, dat, tim, dattim, fract, dbl, decim) VALUES (?,?,?,?,?,?,?,?)",
+            { Tilc::Data::EDBFT_VARCHAR, Tilc::Data::EDBFT_INT, Tilc::Data::EDBFT_DATE, Tilc::Data::EDBFT_TIME, Tilc::Data::EDBFT_DATETIME, Tilc::Data::EDBFT_FLOAT, Tilc::Data::EDBFT_DOUBLE, Tilc::Data::EDBFT_DECIMAL},
+            { "TestowyUser #2", "22", "2026-02-02", "12:22:22", "2026-01-02 12:22:22", "22.22", "22.22222", "22222.22"}
+        );
+        DB.Insert("INSERT INTO users (name, age, dat, tim, dattim, fract, dbl, decim) VALUES (?,?,?,?,?,?,?,?)",
+            { Tilc::Data::EDBFT_VARCHAR, Tilc::Data::EDBFT_INT, Tilc::Data::EDBFT_DATE, Tilc::Data::EDBFT_TIME, Tilc::Data::EDBFT_DATETIME, Tilc::Data::EDBFT_FLOAT, Tilc::Data::EDBFT_DOUBLE, Tilc::Data::EDBFT_DECIMAL},
+            { "TestowyUser #3", "33", "2026-01-03", "12:33:33", "2026-01-03 12:33:33", "33.33", "33.33333", "33333.33"}
+        );
+        DB.Select("SELECT * FROM users", Rows);
+        for (size_t i = 0; i < Rows.size(); ++i)
+        {
+            for (size_t j = 0; j < Rows[i].size(); ++j)
+            {
+                std::cout << Rows[i][j] << " | ";
+            }
+            std::cout << std::endl;
+        }
     }
 
+    Tilc::CleanupTilc();
     return 0;
 }
--- examples/09_Db/src/MariaDB_22_PreparedStatement_From_Tilc.cpp
+++ examples/09_Db/src/MariaDB_22_PreparedStatement_From_Tilc.cpp
@@ -1,42 +1,52 @@
 #include <iostream>
+#include "Tilc/Tilc.h"
 #include "Tilc/Utils/ExtString.h"
 #include "Tilc/Data/DbMySQL.h"
 
-constexpr const char* HOST_NAME{ "192.168.56.2" };
+constexpr const char* HOST_NAME{ "192.168.56.102" };
 constexpr const char* DB_NAME{ "testdb" };
 constexpr const char* USER_NAME{ "testuser" };
 constexpr const char* PASSWD_NAME{ "testpasswd" };
 
 int main(int argc, char* argv[])
 {
-    Tilc::Data::TDBMySQL DB(HOST_NAME, DB_NAME, USER_NAME, PASSWD_NAME);
-    Tilc::Data::TDBDataRows Rows;
+    Tilc::InitTilc();
 
-    DB.Select("DROP TABLE IF EXISTS products", Rows);
-    DB.Select("CREATE TABLE products(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(128), price DECIMAL(10,2), description TEXT)", Rows);
-    
-    for (int i = 1; i <= 9; ++i)
+    // IMPORTANT!!! Variable Tilc::Data::TDBMySQL DB must be in inner scope, to do proper close db. Because if it would be in main scope, function Tilc::Cleanup
+    // would be called before DB destructor. And in destructor would be call to unloaded DLL.
     {
-        std::string ProductName = "Prduct" + std::to_string(i);
-        std::string ProductDescription = std::format("Krótki opis dla produktu: {}\nLinijka1\nLinijka 2", ProductName);
-        DB.Insert("INSERT INTO products(name, price, description) VALUES (?,?,?)", {},
-            { ProductName, std::to_string(111.11 * i), ProductDescription }
-        );
+
+        Tilc::Data::TDBMySQL DB(HOST_NAME, DB_NAME, USER_NAME, PASSWD_NAME);
+        Tilc::Data::TDBDataRows Rows;
+
+        DB.ExecQuery("DROP TABLE IF EXISTS products", {}, {});
+        DB.CreateTable("CREATE TABLE products(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(128), price DECIMAL(10,2), description TEXT)");
+
+        for (int i = 1; i <= 9; ++i)
+        {
+            std::string ProductName = "Product" + std::to_string(i);
+            std::string ProductDescription = std::format("Krótki opis dla produktu: {}\nLinijka1\nLinijka 2", ProductName);
+            DB.Insert("INSERT INTO products(name, price, description) VALUES (?,?,?)",
+                { Tilc::Data::EDBFT_VARCHAR, Tilc::Data::EDBFT_DECIMAL, Tilc::Data::EDBFT_TEXT },
+                { ProductName, std::to_string(111.11 * i), ProductDescription }
+            );
+        }
+
+        DB.Update("UPDATE products set price=price/2", {}, {});
+        DB.Update("UPDATE products SET price=? WHERE id=?",
+            { Tilc::Data::EDBFT_DECIMAL, Tilc::Data::EDBFT_INT },
+            { std::to_string(99999.99), std::to_string(5) });
+        DB.Delete("DELETE FROM products WHERE id=?", 6);
+
+        DB.Select("SELECT * FROM products", Rows);
+        for (size_t i = 0; i < Rows.size(); ++i)
+        {
+            std::cout << Rows[i][0] << " | " << Rows[i][1] << " | " << Rows[i][2] << std::endl;
+            std::cout << "Description:" << std::endl;
+            std::cout << Rows[i][3] << std::endl << std::endl;
+        }
     }
 
-    DB.Update("UPDATE products set price=price/2", {}, {});
-    DB.Update("UPDATE products SET price=? WHERE id=?", {}, {std::to_string(99999.99), std::to_string(5) });
-    DB.Delete("DELETE FROM products WHERE id=?", 6);
-
-    DB.Select("SELECT * FROM products", Rows);
-    for (size_t i = 0; i < Rows.size(); ++i)
-    {
-        std::cout << Rows[i][0] << " | " << Rows[i][1] << " | " << Rows[i][2] << std::endl;
-        std::cout << "Description:" << std::endl;
-        std::cout << Rows[i][3] << std::endl << std::endl;
-    }
-
-    DB.Select("DROP TABLE IF EXISTS products", Rows);
-
+    Tilc::CleanupTilc();
     return 0;
 }
