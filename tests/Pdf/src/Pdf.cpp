e792ad4ab57a4319c091c2f9cab08e953d15eaa8
Marcin Załęczny <mzaleczny@gmail.com>
2026-08-22 14:32:34 +0200

    Added sample program for testing TPDF class.

---
+++ tests/Pdf/src/Pdf.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Tilc/Documents/Pdf/Pdf.h"
+#include "Tilc/Utils/ExtString.h"
+
+int main(int argc, char* argv[])
+{
+    Tilc::Documents::Pdf::TPDF Pdf("document.pdf");
+
+    return 0;
+}
