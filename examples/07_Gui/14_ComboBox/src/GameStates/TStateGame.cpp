6157fe097569d50a964f0b6f79be9717b67aa35b
Marcin Załęczny <mzaleczny@gmail.com>
2026-08-24 11:55:32 +0200

    Added program for testing combooboxes.

---
+++ examples/07_Gui/14_ComboBox/src/GameStates/TStateGame.cpp
@@ -0,0 +1,86 @@
+#include "GameStates/TStateGame.h"
+#include "Tilc/StateManager.h"
+#include "Tilc/Window.h"
+#include "Tilc/Game.h"
+#include "Tilc/Gui/TextBox.h"
+#include "Tilc/Gui/StyledWindow.h"
+#include "Tilc/Gui/ComboBox.h"
+#include "Tilc/Gui/Option.h"
+#include "Tilc/Gui/Font.h"
+#include "Tilc/Gui/Theme.h"
+
+TStateGame::TStateGame(Tilc::TStateManager* StateManager) : Tilc::TBaseState(StateManager, true)
+{
+    Tilc::Gui::TTheme* t = Tilc::GameObject->GetContext()->m_Theme;
+    Tilc::GameObject->InitGuiMode();
+
+    Tilc::GameObject->GetContext()->m_Window->m_TopmostWindow = new Tilc::Gui::TStyledWindow(
+        nullptr,
+        "MainWindow",
+        { 0, 0, static_cast<float>(Tilc::GameObject->m_Window->GetWindowWidth()) , static_cast<float>(Tilc::GameObject->m_Window->GetWindowHeight()) }
+    );
+    Tilc::GameObject->GetContext()->m_Window->m_TopmostWindow->SetText("Przykładowe okienko");
+
+    float x = 10.0f;
+    float y = 35.0f;
+    float w = 240.0f;
+    float h = 25.0f;
+    Tilc::Gui::TStyledWindow* wnd = Tilc::GameObject->GetContext()->m_Window->m_TopmostWindow;
+    Tilc::Gui::TComboBox* cmb = new Tilc::Gui::TComboBox(wnd, "combobox1", { x, y, w, h }, "[Wybierz wartość]",
+        {"Opcja numer 1", "Opcja numer 2", "Opcja numer 3", "Opcja numer 4", "Opcja numer 5", "Opcja numer 6", "Opcja numer 7", "Opcja numer 8"}
+    );
+}
+
+TStateGame::~TStateGame()
+{
+	if (m_TextBox)
+	{
+		delete m_TextBox;
+		m_TextBox = nullptr;
+	}
+}
+
+void TStateGame::OnCreate()
+{
+	Tilc::TEventManager* evMgr = Tilc::GameObject->GetContext()->m_EventManager;
+	if (evMgr)
+	{
+		evMgr->AddCallback(Tilc::EStateType::Game, "Key_Escape", &TStateGame::KeyEscapePressed, this);
+	}
+	m_Elapsed = 0.0f;
+
+    Tilc::TWindow* Window = Tilc::GameObject->GetContext()->m_Window;
+    if (Window->m_TopmostWindow)
+    {
+        Window->m_TopmostWindow->AllowResizing(true);
+    }
+}
+
+void TStateGame::OnDestroy()
+{
+	Tilc::TEventManager* evMgr = Tilc::GameObject->GetContext()->m_EventManager;
+	if (evMgr)
+	{
+		evMgr->RemoveCallback(Tilc::EStateType::Game, "Key_Escape");
+	}
+}
+
+void TStateGame::Update(float DeltaTime)
+{
+	static Tilc::TWindow* Window = Tilc::GameObject->GetContext()->m_Window;
+
+	m_Elapsed += DeltaTime;
+}
+
+void TStateGame::Draw()
+{
+	static Tilc::TWindow* Window = Tilc::GameObject->GetContext()->m_Window;
+	static SDL_Renderer* Renderer = Window->GetRenderer();
+    
+    SDL_RenderClear(Renderer);
+    Window->m_TopmostWindow->Draw();
+}
+
+void TStateGame::KeyEscapePressed(Tilc::TEventDetails* Details)
+{
+}
