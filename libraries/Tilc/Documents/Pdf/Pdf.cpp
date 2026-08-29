bdcced5cfc33dbc0d275967deca5fa528f4193c9
Marcin Załęczny <mzaleczny@gmail.com>
2026-08-29 14:19:37 +0200

    Added parsing of XRefTable and trailer. Added function for reading specified object.

--- libraries/Tilc/Documents/Pdf/Pdf.cpp
+++ libraries/Tilc/Documents/Pdf/Pdf.cpp
@@ -1,23 +1,27 @@
 #include "Tilc/Documents/Pdf/Pdf.h"
-#include "Tilc/Utils/FileUtils.h"
 #include <iostream>
+#include <sstream>
+#include <string>
 
 Tilc::Documents::Pdf::TPDF::TPDF(const char* FilePath)
     : m_FilePath(FilePath)
 {
+    m_PdfFile.OpenFile(m_FilePath.c_str(), std::ios::in | std::ios::binary);
     ReadXRef();
+    ReadRoot();
+    std::cout << std::endl;
+    ReadObj(m_Trailer.m_InfoI1);
 }
 
 Tilc::Documents::Pdf::TPDF::~TPDF()
 {
-    std::cout << "~TPDF()" << std::endl;
 }
 
 void Tilc::Documents::Pdf::TPDF::ReadXRef()
 {
-    Tilc::TFile PdfFile(m_FilePath.c_str(), std::ios::in | std::ios::binary);
+    m_XRefTable.clear();
     // Będziemy wczytywali po 64KB danych z pliku
-    size_t FileSize = PdfFile.GetFileSize();
+    size_t FileSize = m_PdfFile.GetFileSize();
     // Jesli plik jest mniejszy
     if (m_ChunkSize >= FileSize)
     {
@@ -30,9 +34,9 @@ void Tilc::Documents::Pdf::TPDF::ReadXRef()
         // w przeciwnym razie ustawiamy się na -ChunkSize od końca liku
         m_CurrentChunkOffset = FileSize - m_ChunkSize;
     }
-    PdfFile.m_File.seekg(m_CurrentChunkOffset, std::ios::beg);
+    m_PdfFile.m_File.seekg(m_CurrentChunkOffset, std::ios::beg);
 
-    if (PdfFile.Read(m_ChunkSize, m_Content) > 0)
+    if (m_PdfFile.Read(m_ChunkSize, m_Content) > 0)
     {
         size_t Pos = m_Content.StrPos("\nstartxref\n", m_ChunkSize - 64);
         if (Pos != std::string::npos)
@@ -47,16 +51,217 @@ void Tilc::Documents::Pdf::TPDF::ReadXRef()
                 {
                     m_CurrentChunkOffset = m_StartXRef;
                     m_StartXRefInBuffer = 0;
-                    PdfFile.m_File.seekg(m_CurrentChunkOffset, std::ios::beg);
-                    PdfFile.Read(m_ChunkSize, m_Content);
+                    m_PdfFile.m_File.seekg(m_CurrentChunkOffset, std::ios::beg);
+                    m_PdfFile.Read(m_ChunkSize, m_Content);
                 }
                 else
                 {
                     m_StartXRefInBuffer = m_StartXRef;
                 }
                 
-                std::cout << m_Content.substr(m_StartXRefInBuffer, 1024) << std::endl;
+                //std::cout << m_Content.substr(m_StartXRefInBuffer, 1024) << "\n\n" << std::endl;
+                ReadXRef_ReadItems();
+                //for (size_t i = 0; i < m_XRefTable.size(); ++i)
+                //{
+                //    std::cout << "XRefItem: " << std::get<0>(m_XRefTable[i]) << " " << std::get<1>(m_XRefTable[i]) << " " << std::get<2>(m_XRefTable[i]) << std::endl;
+                //}
+                ReadTrailer();
+                //std::cout << m_Trailer << std::endl;
             }
         }
     }
+}
+
+int Tilc::Documents::Pdf::TPDF::ReadXRef_AdjustContentData()
+{
+    // Pobieramy pozycję stopki
+    size_t pos = m_Content.find("trailer");
+    if (pos == std::string::npos)
+    {
+        // Jeśli nie znaleziono jej w buforze, to nie cała Xref mieści się w buforze. Musimy więc obciąć bufor do ostatniego wystąpienia "\n", ale włączając ten znak.
+        // Będziemy musieli więc wczytywać kolejne chunki danych w miarę przetwarzania bufora i xreftable
+        pos = m_Content.rfind("\n");
+        if (pos == std::string::npos)
+        {
+            return -1;
+        }
+        m_Content = m_Content.substr(0, pos + 1);
+    }
+    return 0;
+}
+
+int Tilc::Documents::Pdf::TPDF::ReadXRef_ReadItems()
+{
+    // Pobieramy pozycję stopki
+    size_t pos = m_Content.find("xref\n");
+    if (pos == std::string::npos) return -2;
+    pos += 5;
+    if (ReadXRef_AdjustContentData() < 0) return -1;
+    Tilc::TExtString Line, XRefTableString(&m_Content[pos]);
+    std::istringstream is, isLine;
+    int i1, ItemsCount, Offset, i2;
+    char ch;
+
+    is.str(XRefTableString);
+    std::getline(is, Line);
+    isLine.str(Line);
+    isLine >> i1 >> ItemsCount;
+    if (ItemsCount < 1)
+    {
+        return -3;
+    }
+
+    m_XRefTable.reserve(ItemsCount);
+    for (int i = 0; i < ItemsCount; ++i)
+    {
+        std::getline(is, Line);
+        if (Line == "trailer")
+        {
+            if (i != ItemsCount - 1)
+            {
+                return -4;
+            }
+            break;
+        }
+        else if (Line.empty())
+        {
+            // doczytujemy następny chunk danych
+            m_PdfFile.m_File.seekg(m_CurrentChunkOffset + m_Content.size(), std::ios::beg);
+            m_CurrentChunkOffset += m_Content.size();
+            m_PdfFile.Read(m_ChunkSize, m_Content);
+            if (ReadXRef_AdjustContentData() < 0) return -5;
+            is.str(m_Content);
+            std::getline(is, Line);
+        }
+        isLine.str(Line);
+        isLine.clear();
+        isLine >> Offset >> i2 >> ch;
+        m_XRefTable.emplace_back(Offset, i2, ch);
+    }
+
+    return 0;
+}
+
+int Tilc::Documents::Pdf::TPDF::ReadTrailer()
+{
+    size_t pos = m_Content.find("trailer\n");
+    size_t TrailerStartPos = pos;
+    while (pos == std::string::npos)
+    {
+        m_PdfFile.m_File.seekg(m_CurrentChunkOffset + m_Content.size(), std::ios::beg);
+        m_CurrentChunkOffset += m_Content.size();
+        m_PdfFile.Read(m_ChunkSize, m_Content);
+        pos = m_Content.find("trailer\n");
+        if (m_Content.empty())
+        {
+            break;
+        }
+    }
+    if (pos == std::string::npos)
+    {
+        return -1;
+    }
+
+    pos = m_Content.find(">>", pos);
+    if (pos == std::string::npos)
+    {
+        m_PdfFile.m_File.seekg(m_CurrentChunkOffset + TrailerStartPos, std::ios::beg);
+        m_CurrentChunkOffset += TrailerStartPos;
+        TrailerStartPos = 0;
+        m_PdfFile.Read(m_ChunkSize, m_Content);
+        pos = m_Content.find("trailer\n");
+    }
+    pos = m_Content.find(">>");
+    if (pos == std::string::npos)
+    {
+        return -2;
+    }
+
+    Tilc::TExtString TrailerString(&m_Content[TrailerStartPos]);
+    std::istringstream is(TrailerString), isLine;
+    Tilc::TExtString Line, Item;
+    while (std::getline(is, Line))
+    {
+        if (Line.StartsWith("<</Size"))
+        {
+            isLine.str(Line);
+            isLine.clear();
+            isLine >> Item; // "<</Size"
+            isLine >> Item;
+            // zmienna poniżej powinna zawierać ilość obiektów czyli wpisów w tabeli m_XRefTable
+            m_Trailer.m_SizeI1 = ReadIntValueFromPairBefore(Item, "Root");
+            if (m_Trailer.m_SizeI1 != m_XRefTable.size())
+            {
+                return -3;
+            }
+            isLine >> m_Trailer.m_RootObjectNumber >> m_Trailer.m_SizeI2 >> m_Trailer.m_SizeI3;
+        }
+        else if (Line.StartsWith("/Info"))
+        {
+            isLine.str(Line);
+            isLine.clear();
+            isLine >> Item; // "/Info"
+            isLine >> m_Trailer.m_InfoI1 >> m_Trailer.m_InfoI2 >> m_Trailer.m_InfoI3;
+        }
+        else if (Line.StartsWith("/ID"))
+        {
+            isLine.str(Line);
+            isLine.clear();
+
+            isLine >> Item; // "/ID"
+            isLine >> Item; // "["
+            isLine >> Item; // "ID1"
+            Item.StrReplace("<", "");
+            Item.StrReplace(">", "");
+            m_Trailer.m_ID[0] = Item;
+
+            std::getline(is, Line);
+            isLine.str(Line);
+            isLine.clear();
+
+            isLine >> Item; // "ID2"
+            Item.StrReplace("<", "");
+            Item.StrReplace(">", "");
+            m_Trailer.m_ID[1] = Item;
+        }
+        else if (Line.StartsWith("/DocChecksum"))
+        {
+            isLine.str(Line);
+            isLine.clear();
+
+            isLine >> Item; // "/DocCheckSum"
+            isLine >> Item; // "ID"
+            Item.StrReplace("/", "");
+            m_Trailer.m_DocChecksum = Item;
+        }
+    }
+    return 0;
+}
+
+void Tilc::Documents::Pdf::TPDF::ReadObj(int ObjNum)
+{
+    //std::cout << "ObjNum: " << ObjNum << " / " << m_XRefTable.size() << std::endl;
+    if (ObjNum >= 0 && ObjNum < m_XRefTable.size())
+    {
+        Tilc::Documents::Pdf::TPdfXRefItem& Root = m_XRefTable[ObjNum];
+        int Offset = std::get<0>(Root);
+        if (Offset < m_CurrentChunkOffset || Offset >= m_CurrentChunkOffset + m_Content.size())
+        {
+            m_PdfFile.m_File.seekg(Offset, std::ios::beg);
+            m_CurrentChunkOffset = Offset;
+            Offset = 0;
+            m_PdfFile.Read(m_ChunkSize, m_Content);
+        }
+        //std::cout << "Offset: " << Offset << std::endl;
+        size_t EndObjPos = m_Content.find("endobj", Offset);
+        if (EndObjPos != std::string::npos)
+        {
+            std::cout << m_Content.substr(Offset, EndObjPos + 6 - Offset) << std::endl;
+        }
+    }
+}
+
+void Tilc::Documents::Pdf::TPDF::ReadRoot()
+{
+    ReadObj(m_Trailer.m_RootObjectNumber);
 }
--- libraries/Tilc/Documents/Pdf/Pdf.h
+++ libraries/Tilc/Documents/Pdf/Pdf.h
@@ -3,6 +3,10 @@
 
 #include "Tilc/DllGlobals.h"
 #include "Tilc/Utils/ExtString.h"
+#include "Tilc/Utils/FileUtils.h"
+
+#include <iostream>
+#include <tuple>
 
 namespace Tilc
 {
@@ -10,18 +14,67 @@ namespace Tilc
     {
         namespace Pdf
         {
+            using TPdfXRefItem = std::tuple<int, int, char>;
+            struct TPdfTrailer
+            {
+                int m_RootObjectNumber;
+                int m_SizeI1, m_SizeI2;
+                char m_SizeI3;
+                int m_InfoI1, m_InfoI2;
+                char m_InfoI3;
+                Tilc::TExtString m_ID[2];
+                Tilc::TExtString m_DocChecksum;
+
+                friend std::ostream& operator<<(std::ostream& out, const TPdfTrailer& trailer)
+                {
+                    out << "Size: " << trailer.m_SizeI1 << "/Root" << " " << trailer.m_RootObjectNumber << " " << trailer.m_SizeI2 << " " << trailer.m_SizeI3 << std::endl;
+                    out << "Info: " << trailer.m_InfoI1 << " " << trailer.m_InfoI2 << " " << trailer.m_InfoI3 << std::endl;
+                    out << "ID: " << trailer.m_ID[0] << " " << trailer.m_ID[1] << std::endl;
+                    out << "DocCheckSum: " << trailer.m_DocChecksum;
+                    return out;
+                }
+            };
+
             class DECLSPEC TPDF
             {
             public:
                 TPDF(const char* FilePath);
                 virtual ~TPDF();
             protected:
-                size_t m_StartXRef{}, m_StartXRefInBuffer;
+                Tilc::TFile m_PdfFile;
+                // offset of XRef table in pdf file
+                size_t m_StartXRef{};
+                // offset of XRef table in read in buffer stored in m_Content
+                size_t m_StartXRefInBuffer;
+                // offset of current chunk read in and stored in m_Content related to begin of pdf file
                 size_t m_CurrentChunkOffset{};
                 size_t m_ChunkSize{ 64 * 1024 };
+                // File path that is being processed
                 Tilc::TExtString m_FilePath;
+                // currrent chunk, data buffer read in from pdf file
                 Tilc::TExtString m_Content;
+                // XRefTable items
+                std::vector<TPdfXRefItem> m_XRefTable;
+                TPdfTrailer m_Trailer;
+
+                inline int ReadIntValueFromPairBefore(Tilc::TExtString Item, const Tilc::TExtString& ValName)
+                {
+                    if (size_t pos = Item.find("/" + ValName); pos != std::string::npos)
+                    {
+                        Item = Item.substr(0, pos);
+                        return std::atoi(Item.c_str());
+                    }
+                    return static_cast<int>(std::string::npos);
+                }
+                // Read XRefTable
                 void ReadXRef();
+                int ReadXRef_AdjustContentData();
+                int ReadXRef_ReadItems();
+
+                int ReadTrailer();
+
+                void ReadObj(int ObjNum);
+                void ReadRoot();
             };
         }
     }
