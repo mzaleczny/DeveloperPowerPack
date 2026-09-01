022e0e195df8e7bf68ed8bfe344f87265cb953b7
Marcin Załęczny <mzaleczny@gmail.com>
2026-09-01 14:45:28 +0200

    Added some controls to json string.

--- examples/07_Gui/15_ReadingWindowLayoutFromJson/src/GameStates/TStateGame.cpp
+++ examples/07_Gui/15_ReadingWindowLayoutFromJson/src/GameStates/TStateGame.cpp
@@ -6,6 +6,7 @@
 #include "Tilc/Gui/StyledWindow.h"
 #include "Tilc/Gui/ComboBox.h"
 #include "Tilc/Gui/Listbox.h"
+#include "Tilc/Gui/Button.h"
 #include "Tilc/Gui/MultiColumnListbox.h"
 #include "Tilc/Gui/Option.h"
 #include "Tilc/Gui/Font.h"
@@ -36,7 +37,7 @@ TStateGame::TStateGame(Tilc::TStateManager* StateManager) : Tilc::TBaseState(Sta
             "x": 0,
             "y": 0,
             "width": 1200,
-            "height": 900,
+            "height": 800,
             "caption": "Application demonstrating reading layout from json",
             "items": [
                 {
@@ -45,8 +46,157 @@ TStateGame::TStateGame(Tilc::TStateManager* StateManager) : Tilc::TBaseState(Sta
                     "x": 10,
                     "y": 35,
                     "width": 150,
-                    "height": 25,
+                    "height": 30,
                     "text": "Włącz nagłówek okna"
+                },
+                {
+                    "name": "btnTurnOffCaption",
+                    "type": "button",
+                    "x": 170,
+                    "y": 35,
+                    "width": 150,
+                    "height": 30,
+                    "text": "Wyłącz nagłówek okna"
+                },
+                {
+                    "name": "btnTurnOnBackground",
+                    "type": "button",
+                    "x": 330,
+                    "y": 35,
+                    "width": 150,
+                    "height": 25,
+                    "text": "Włącz tło okna"
+                },
+                {
+                    "name": "btnTurnOffBackground",
+                    "type": "button",
+                    "x": 490,
+                    "y": 35,
+                    "width": 150,
+                    "height": 25,
+                    "text": "Wyłącz tło okna"
+                },
+                {
+                    "name": "lblName",
+                    "type": "label",
+                    "x": 10,
+                    "y": 70,
+                    "width": 150,
+                    "height": 25,
+                    "text": "Imię:",
+                    "color": "#ffffffff"
+                },
+                {
+                    "name": "edtName",
+                    "type": "textfield",
+                    "x": 160,
+                    "y": 70,
+                    "width": 150,
+                    "height": 25
+                },
+                {
+                    "name": "lblLastName",
+                    "type": "label",
+                    "x": 10,
+                    "y": 100,
+                    "width": 150,
+                    "height": 25,
+                    "text": "Nazwisko:",
+                    "color": "#ffffffff"
+                },
+                {
+                    "name": "edtLastName",
+                    "type": "textfield",
+                    "x": 160,
+                    "y": 100,
+                    "width": 150,
+                    "height": 25
+                },
+                {
+                    "name": "lblDescription",
+                    "type": "label",
+                    "x": 330,
+                    "y": 70,
+                    "width": 50,
+                    "height": 25,
+                    "text": "Opis:",
+                    "color": "#ffffffff"
+                },
+                {
+                    "name": "txtDescription",
+                    "type": "multiline-textfield",
+                    "x": 380,
+                    "y": 70,
+                    "width": 450,
+                    "height": 60
+                },
+                {
+                    "name": "grid",
+                    "type": "grid",
+                    "x": 10,
+                    "y": 140,
+                    "width": 550,
+                    "height": 450
+                },
+                {
+                    "name": "chk",
+                    "type": "checkbox",
+                    "x": 10,
+                    "y": 600,
+                    "width": 100,
+                    "height": -1,
+                    "checked-state-text": "Checkbox CHECKED",
+                    "unchecked-state-text": "Checkbox unchecked"
+                },
+                {
+                    "name": "opt11",
+                    "type": "option",
+                    "x": 10,
+                    "y": 640,
+                    "width": 100,
+                    "height": -1,
+                    "group": 1,
+                    "text": "Option #1"
+                },
+                {
+                    "name": "opt12",
+                    "type": "option",
+                    "x": 120,
+                    "y": 640,
+                    "width": 100,
+                    "height": -1,
+                    "group": 1,
+                    "text": "Option #2"
+                },
+                {
+                    "name": "opt21",
+                    "type": "option",
+                    "x": 10,
+                    "y": 670,
+                    "width": 150,
+                    "height": -1,
+                    "group": 2,
+                    "text": "Option #1 - Grupa 2"
+                },
+                {
+                    "name": "opt22",
+                    "type": "option",
+                    "x": 170,
+                    "y": 670,
+                    "width": 150,
+                    "height": -1,
+                    "group": 2,
+                    "text": "Option #2 - Grupa 2"
+                },
+                {
+                    "name": "opt23",
+                    "type": "option",
+                    "x": 330,
+                    "y": 670,
+                    "width": 150,
+                    "height": -1,
+                    "group": 2,
+                    "text": "Option #3 - Grupa 2"
                 }
             ]
         }
@@ -55,7 +205,8 @@ TStateGame::TStateGame(Tilc::TStateManager* StateManager) : Tilc::TBaseState(Sta
 )##");
     Tilc::Gui::TJsonLayoutFile layout;
     layout.loadLayout(Json, wnd);
-    wnd->GetChild("btnTurnOnCaption")->SetText("Włącz nagłówek okna");
+
+    Tilc::Gui::TButton* Button = new Tilc::Gui::TButton(wnd, "btn11", { 680.0f, 35.0f, 150.0f, 25.0f }, "Przycisk #11");
 }
 
 TStateGame::~TStateGame()
