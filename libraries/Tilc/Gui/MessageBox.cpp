3696659f5b9d32afe3872d6d4284099474918471
Marcin Załęczny <mzaleczny@gmail.com>
2026-09-02 09:49:08 +0200

    Added outputting Messages to stdout.

--- libraries/Tilc/Gui/MessageBox.cpp
+++ libraries/Tilc/Gui/MessageBox.cpp
@@ -3,42 +3,49 @@
 
 DECLSPEC int Tilc::Gui::MessageBox(Tilc::TExtString caption, Tilc::TExtString message)
 {
+    SDL_Log("%s", message.c_str());
     int result = 0;//::MessageBoxW(GetActiveWindow(), message.getBuffer(), caption.getBuffer(), MB_OK | MB_ICONINFORMATION);
     return result;
 }
 
 DECLSPEC int Tilc::Gui::AlertBox(Tilc::TExtString message)
 {
+    SDL_Log("Uwaga: %s", message.c_str());
     int result = 0;//::MessageBoxW(GetActiveWindow(), message.getBuffer(), L"Uwaga", MB_OK | MB_ICONWARNING);
     return result;
 }
 
 DECLSPEC int Tilc::Gui::WarningBox(Tilc::TExtString message)
 {
+    SDL_Log("Uwaga: %s", message.c_str());
     int result = 0;//::MessageBoxW(GetActiveWindow(), message.getBuffer(), L"Uwaga", MB_OK | MB_ICONWARNING);
     return result;
 }
 
 DECLSPEC int Tilc::Gui::QuestionBox(Tilc::TExtString message)
 {
+    SDL_Log("Pytanie: %s", message.c_str());
     int result = 0;//::MessageBoxW(GetActiveWindow(), message.getBuffer(), L"Pytanie", MB_YESNO | MB_ICONQUESTION);
     return result;
 }
 
 DECLSPEC int Tilc::Gui::QuestionCancelBox(Tilc::TExtString message)
 {
+    SDL_Log("Pytania: %s", message.c_str());
     int result = 0;//::MessageBoxW(GetActiveWindow(), message.getBuffer(), L"Pytanie", MB_YESNOCANCEL | MB_ICONQUESTION);
     return result;
 }
 
 DECLSPEC int Tilc::Gui::ErrorBox(Tilc::TExtString message)
 {
+    SDL_Log("Błąd: %s", message.c_str());
     int result = 0;//::MessageBoxW(GetActiveWindow(), message.getBuffer(), L"Błąd", MB_OK | MB_ICONERROR);
     return result;
 }
 
 DECLSPEC int Tilc::Gui::InformationBox(Tilc::TExtString message)
 {
+    SDL_Log("Informacja: %s", message.c_str());
     int result = 0;//::MessageBoxW(GetActiveWindow(), message.getBuffer(), L"Informacja", MB_OK | MB_ICONINFORMATION);
     return result;
 }
