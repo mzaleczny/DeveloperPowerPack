1d9cb8a26663f88358193f3d99261ea61b4321da
Marcin Załęczny <mzaleczny@gmail.com>
2026-09-18 17:02:53 +0200

    Transforming email validator functions to template ones to allow string and widestring versions.

--- libraries/Tilc/Utils/Validation.h
+++ libraries/Tilc/Utils/Validation.h
@@ -7,9 +7,12 @@
 namespace Tilc
 {
 	using namespace std::string_literals;
+	template <typename CharT>
+	using tstring = std::basic_string<CharT, std::char_traits<CharT>, std::allocator<CharT>>;
 
 	// Use: std::cout << std::setw(30) << std::left	<< email << " : " << (IsValidEmailFormat(email) ? "valid" : "invalid") << '\n';
-	inline bool IsValidEmailFormat(Tilc::TExtString const & email)
+	template <typename CharT>
+	bool IsValidEmailFormat(tstring<CharT> const & email)
 	{
 		// std::regex_constants::icase - extra argument that means that we want case insensitive match
 		auto rx = std::regex{ R"(^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$)"s, std::regex_constants::icase };
@@ -17,7 +20,8 @@ namespace Tilc
 	}
 
 	// Use: auto[valid, localpart, hostname, dnslabel] = IsValidEmailFormatWithResult(email);
-	inline std::tuple<bool, Tilc::TExtString, Tilc::TExtString, Tilc::TExtString> IsValidEmailFormatWithResult(Tilc::TExtString const & email)
+	template <typename CharT>
+	inline std::tuple<bool, tstring<CharT>, tstring<CharT>, tstring<CharT>> IsValidEmailFormatWithResult(tstring<CharT> const & email)
 	{
 		auto rx = std::regex{ R"(^([A-Z0-9._%+-]+)@([A-Z0-9.-]+)\.([A-Z]{2,})$)"s, std::regex_constants::icase };
 		auto result = std::smatch{};
