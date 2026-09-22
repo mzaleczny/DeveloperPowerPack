a026e0a5733a234525d96f93c7443439740f4400
Marcin Załęczny <mzaleczny@gmail.com>
2026-09-22 14:59:35 +0200

    Fix for calculating clicked item. It now take into account optional visible header.

--- libraries/Tilc/Gui/Listbox.cpp
+++ libraries/Tilc/Gui/Listbox.cpp
@@ -753,14 +753,21 @@ bool Tilc::Gui::TListbox::OnMouseButtonDown(const SDL_Event& event)
         CaptureMouse(this);
 
         Tilc::Gui::TTheme* t = GetTheme();
-        int ClickedItem = (event.button.y - m_RealPosition.y - t->listbox_frame_top_rc.h);
-        if (m_VScrollBar)
+        float HeaderHeight = 0.0;
+        if (!m_Header.m_Value.empty())
         {
-            ClickedItem += m_TopItemIndex * m_MeasuredTextSize.y;
+            HeaderHeight = t->listbox_header_middle_rc.h;
         }
-        ClickedItem /= m_MeasuredTextSize.y;
-        SelectItem(ClickedItem);
-
+        int ClickedItem = (event.button.y - HeaderHeight - m_RealPosition.y - t->listbox_frame_top_rc.h - m_Padding);
+        if (ClickedItem >= 0)
+        {
+            if (m_VScrollBar)
+            {
+                ClickedItem += m_TopItemIndex * m_MeasuredTextSize.y;
+            }
+            ClickedItem /= m_MeasuredTextSize.y;
+            SelectItem(ClickedItem);
+        }
         return true;
     }
 
